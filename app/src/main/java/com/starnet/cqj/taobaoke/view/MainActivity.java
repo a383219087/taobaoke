469877d6e3ca8aab58163c1966682ca00ae56beb
johnChen <383219087@qq.com>
2017-10-31 17:39:10 +0800

    add library

--- app/src/main/java/com/starnet/cqj/taobaoke/view/MainActivity.java
+++ app/src/main/java/com/starnet/cqj/taobaoke/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.starnet.cqj.taobaoke;
+package com.starnet.cqj.taobaoke.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.starnet.cqj.taobaoke.R;
 
 public class MainActivity extends AppCompatActivity {
 
