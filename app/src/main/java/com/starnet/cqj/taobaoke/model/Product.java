75bffda00aadaf61aa6de63cb34164acbd89bda7
johnChen <383219087@qq.com>
2017-11-08 17:17:27 +0800

    add product item

---
+++ app/src/main/java/com/starnet/cqj/taobaoke/model/Product.java
@@ -0,0 +1,113 @@
+package com.starnet.cqj.taobaoke.model;
+
+public class Product {
+
+    private String id;
+    private String origin_price;
+    private String price;
+    private int itemid;
+    private String coupon_fee;
+    private String url;
+    private int sell;
+    private int istmall;
+    private int score;
+    private String icon;
+    private String itempic;
+    private String title;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrigin_price() {
+        return origin_price;
+    }
+
+    public void setOrigin_price(String origin_price) {
+        this.origin_price = origin_price;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(int itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getCoupon_fee() {
+        return coupon_fee;
+    }
+
+    public void setCoupon_fee(String coupon_fee) {
+        this.coupon_fee = coupon_fee;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getSell() {
+        return sell;
+    }
+
+    public void setSell(int sell) {
+        this.sell = sell;
+    }
+
+    public int getIstmall() {
+        return istmall;
+    }
+
+    public void setIstmall(int istmall) {
+        this.istmall = istmall;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getItempic() {
+        return itempic;
+    }
+
+    public void setItempic(String itempic) {
+        this.itempic = itempic;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ app/src/main/java/com/starnet/cqj/taobaoke/view/activity/ProductDetailActivity.java
@@ -0,0 +1,19 @@
+package com.starnet.cqj.taobaoke.view.activity;
+
+import com.starnet.cqj.taobaoke.R;
+
+/**
+ * Created by Administrator on 2017/11/08.
+ */
+
+public class ProductDetailActivity extends BaseActivity {
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_product_detail;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
