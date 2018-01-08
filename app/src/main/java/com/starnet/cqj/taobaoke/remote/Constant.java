733ca18beec5b51c0a29459efbc938b907842d89
johnChen <383219087@qq.com>
2018-01-08 14:00:07 +0800

    edit base url

--- app/src/main/java/com/starnet/cqj/taobaoke/remote/Constant.java
+++ app/src/main/java/com/starnet/cqj/taobaoke/remote/Constant.java
@@ -6,25 +6,25 @@ package com.starnet.cqj.taobaoke.remote;
 
 public class Constant {
 
-    public static final String BASE_URL ="http://api.agpiu.cn/";
+    public static final String BASE_URL = "http://api.taocncbk.com/";
 
-    public static final String IMAGE_URL ="http://api.agpiu.cn";
+    public static final String IMAGE_URL = "http://api.taocncbk.com";
 
-    public static final String COMMON_PREFERENCE_NAME ="tdr";
+    public static final String COMMON_PREFERENCE_NAME = "tdr";
 
-    public static final String HEADER_PREFIX="Bearer ";
+    public static final String HEADER_PREFIX = "Bearer ";
 
-    public static final String HELP_DETAIL_PREFIX ="http://api.bfija.cn/helpD/";
+    public static final String HELP_DETAIL_PREFIX = BASE_URL + "helpD/";
 
-    public static final String HOT_DETAIL_PREFIX ="http://api.bfija.cn/hot/hotD/";
+    public static final String HOT_DETAIL_PREFIX = BASE_URL + "hot/hotD/";
 
     public static final int MAIN_TIP_GET_INTERVAL = 15000;
 
     public static final String KEY_TOKEN = "token";
-    public static final String KEY_TOKEN_DATE ="token_save_date";
+    public static final String KEY_TOKEN_DATE = "token_save_date";
 
     public static final int TOKEN_REFRESH_DAY = 3;
     public static final int BANNER_AUTO_TIME = 5000;
 
-    public static final int GET_MEDAL_SERVICE_SECOND =60;
+    public static final int GET_MEDAL_SERVICE_SECOND = 60;
 }
